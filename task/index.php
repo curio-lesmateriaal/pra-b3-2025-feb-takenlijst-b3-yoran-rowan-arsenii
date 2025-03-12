3a3cc3807c2a108662384e2475adef96472a8dc3
Kolimov <60222324+Sasmisl@users.noreply.github.com>
2025-03-12 14:30:05 +0100

    index.php changed

--- task/index.php
+++ task/index.php
@@ -1,4 +1,3 @@
-
 <!doctype html>
 <html lang="nl">
 
@@ -19,10 +18,12 @@
     ?>
 
     <div class="container">
-        <div class="knop-aanmaken">
-            <a href="create.php" id="create" class="aanmaken-tekst">Taak aanmaken</a>
-        </div>
-        
+        <a href="create.php" id="create">Taak aanmaken</a>
+        <form action="../backend/Controllers/taskController.php" method="POST" onsubmit="return confirm('Weet je zeker dat je deze melding wilt verwijderen?');">
+            <input type="hidden" name="action" value="delete">
+            <input type="hidden" name="id" value="<?php echo $id; ?>">
+            <input type="submit" value="Verwijderen">
+        </form>
         <?php if(isset($_GET['msg']))
         {
             echo "<div class='msg'>" . $_GET['msg'] . "</div>";
@@ -31,18 +32,48 @@
         <div class="task-menu">
             <div class="task-block" id="todo">
                 <h2>Todo</h2>
-                <div class="task">
-
-                </div>
+                <?php foreach ($tasks as $task): ?>
+                    <?php if ($task['status'] === 'todo'): ?>
+                        <div class="task">
+                            <p><?php echo $task['titel']; ?></p>
+                            <form action="<?php echo $base_url; ?>/backend/controllers/taskController.php" method="POST" onsubmit="return confirm('Weet je zeker dat je deze taak wilt verwijderen?');">
+                                <input type="hidden" name="action" value="delete">
+                                <input type="hidden" name="id" value="<?php echo $task['id']; ?>">
+                                <input type="submit" value="Verwijderen" class="delete-button">
+                            </form>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </div>
             <div class="task-block" id="doing">
                 <h2>Doing</h2>
-                <div class="task">
-
-                </div>
+                <?php foreach ($tasks as $task): ?>
+                    <?php if ($task['status'] === 'doing'): ?>
+                        <div class="task">
+                            <p><?php echo $task['titel']; ?></p>
+                            <form action="<?php echo $base_url; ?>/backend/controllers/taskController.php" method="POST" onsubmit="return confirm('Weet je zeker dat je deze taak wilt verwijderen?');">
+                                <input type="hidden" name="action" value="delete">
+                                <input type="hidden" name="id" value="<?php echo $task['id']; ?>">
+                                <input type="submit" value="Verwijderen" class="delete-button">
+                            </form>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </div>
             <div class="task-block" id="done">
                 <h2>Done</h2>
+                <?php foreach ($tasks as $task): ?>
+                    <?php if ($task['status'] === 'done'): ?>
+                        <div class="task">
+                            <p><?php echo $task['titel']; ?></p>
+                            <form action="<?php echo $base_url; ?>/backend/controllers/taskController.php" method="POST" onsubmit="return confirm('Weet je zeker dat je deze taak wilt verwijderen?');">
+                                <input type="hidden" name="action" value="delete">
+                                <input type="hidden" name="id" value="<?php echo $task['id']; ?>">
+                                <input type="submit" value="Verwijderen" class="delete-button">
+                            </form>
+                        </div>
+                    <?php endif; ?>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
